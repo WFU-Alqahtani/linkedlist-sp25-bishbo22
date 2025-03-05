bf60c198e1fbd9cd5aed41f8f5147fa25d2d57c9
blair <bishbo22@wfu.edu>
2025-03-05 15:43:04 -0500

    first done

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -18,7 +18,7 @@ public class LinkedList {
         Random rand = new Random();
         Node head = new Node(rand.nextInt(10));
         Node curr = head;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             Node temp = new Node(rand.nextInt(10));
             curr.next = temp;
             curr = curr.next;
@@ -31,6 +31,34 @@ public class LinkedList {
             curr = curr.next;
         }
 
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter one of the values to delete: ");
+        int x = kb.nextInt();
+
+        // deletion
+        if (head.data == x){
+            head = head.next;
+        }
+        Node temp = head;
+        curr = head.next;
+        while (curr != null){
+            if (curr.data == x && curr.next != null){
+                temp.next = curr.next;
+            }
+            if (curr.data == x && curr.next == null){
+                temp.next = null;
+            }
+            curr = curr.next;
+            temp = temp.next;
+        }
+
+
+        //print again
+        curr = head;
+        while (curr != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
     }
 
 }
